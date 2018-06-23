51cc85eb1266341842b4ae6fdc4616ac229dbabd
Char Space <charspacegroup@gmail.com>
2018-06-23 16:17:16 +0630

    save

--- GIIS.ERP.WMS/WMS.svc.cs
+++ GIIS.ERP.WMS/WMS.svc.cs
@@ -9,6 +9,7 @@ using CHARS.HR.COMMON.BLL;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using CHARS.HR.COMMON.General;
 
 namespace GIIS.ERP.WMS
 {
@@ -16,7 +17,7 @@ namespace GIIS.ERP.WMS
     // NOTE: In order to launch WCF Test Client for testing this service, please select WMS.svc or WMS.svc.cs at the Solution Explorer and start debugging.
     public class WMS : IWMS
     {
-       
+        Utility mUtility = new Utility();
         MasterBLL mMasterBLL = new MasterBLL();
         bool SaveStatus = false;
         SqlConnection mConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConString"].ConnectionString);
@@ -40,11 +41,39 @@ namespace GIIS.ERP.WMS
 
         public SysMenu getSysMenu()
         {
+            SysMenu a = new SysMenu();
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("@UserID", "admin");
             DataTable tbl = mMasterBLL.executeSelectProcedure("CS_SP_SELECT_USER_MENU", dic);
-           
-            return  new SysMenu();
+
+            #region "Bind Object"
+            //if (a.Ask == "0")
+            //{
+            //    a.Ask = mUtility.getStrAsk().ToString();
+            //    SaveStatus = true;
+            //}
+            //else
+            //{
+            //    l_Read.Ask = aReadInfo.Ask;
+            //    SaveStatus = false;
+            //}
+
+            //l_Read.Ts = aReadInfo.Ts;
+            //l_Read.UD = aReadInfo.UD;
+            //l_Read.MemberAsk = aReadInfo.MemberAsk;
+            //l_Read.BookAsk = aReadInfo.BookAsk;
+            //l_Read.MarkAsk = aReadInfo.MarkAsk;
+            //l_Read.ReadStatus = aReadInfo.ReadStatus;
+            //l_Read.ReadDate = aReadInfo.ReadDate;
+            //l_Read.ExpiredDate = aReadInfo.ExpiredDate;
+            //l_Read.ExtendTime = aReadInfo.ExtendTime;
+            //l_Read.Remark = aReadInfo.Remark;
+            //l_Read.ReturnDate = aReadInfo.ReturnDate;
+            //l_Read.Libray = aReadInfo.Libray;
+            #endregion
+
+
+            return new SysMenu();
         }
     }
 }
