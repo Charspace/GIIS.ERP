872c1f4789ed403f9e6287f7f583eed734e9975e
TMM\ayeayenyein <ayeaye_nyein@truemoneymyanmar.com>
2018-06-25 22:18:58 +0630

    [aan] 10:18

---
+++ GIIS.ERP.WMS/Cri/SysMenuCri.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace GIIS.ERP.WMS
+{
+    public class SysMenuCri
+    {
+        public SysMenuCri()
+        {
+            setDefaultValue();
+        }
+
+        #region "private member"
+        private string mMenuGpAsk;
+        private string mTS;
+        private string mUD;
+        private string mDisplaySequence;
+        private string mRemark;
+        private string mUser;
+        private string mPWD;
+        private string mMenuAsk;
+        private string mMenuName;
+
+
+        #endregion
+        #region "public member"
+        [DataMember]
+        public string Ask
+        {
+            get { return mMenuAsk; }
+            set { mMenuAsk = value; }
+        }
+        [DataMember]
+        public string MenuName
+        {
+            get { return mMenuName; }
+            set { mMenuName = value; }
+
+        }
+
+
+
+        #endregion
+        #region"Default Property"
+        public void setDefaultValue()
+        {
+            mMenuName = "";
+            mMenuAsk = "";
+        }
+        #endregion
+    }
+}
